ef1c9d22c8dc6916094b579bfe76e8b1bb6f3678
ikalemaj <ikalemaj@client.project5.ch-geni-net.instageni.ku.gpeni.net>
2019-12-04 15:56:30 -0600

    Added communication of image ID to client

--- client_image.py
+++ client_image.py
@@ -3,38 +3,27 @@ import socket, select
 from time import gmtime, strftime
 from random import randint
 
-
-
-def recv_all(connectionSocket):
-    message = ""
-    while True:
-      try:
-        p = connectionSocket.recv(1).decode()
-        message += p
-        if p == "\n":
-          break
-      except:
-        break
-    return(message)
-
-
+# list of image names  used in the experiment
 images = ["apple-touch-icon-144x144-precomposed.png"]
-
+# list of corresponding id, indicating network parameters used 
+# for each image in the experiment
+ids = [1]
 
 
 HOST = '198.248.248.133'
 PORT = 8889
 
 
-for image in images:
+for i in range(len(images)):
+    
+    # establish connection with manager		
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
     sock.connect(server_address)
     
-    myfile = open(image, 'rb')
+    # read in image
+    myfile = open(images[i], 'rb')
     bytes = myfile.read()
-    #bytes += "\n"
-    print "Size of image to be delivered is %s" %len(bytes)
     size = len(bytes)
     
     # send image size to server
@@ -42,27 +31,40 @@ for image in images:
     
     answer = ""
     try:
-      answer = recv_all(sock)
+      answer = sock.recv(4096)
       print 'answer = %s' % answer
     except:
       sock.close()
     
-    # send image to server
+    # send image ID and image to server
     if answer.startswith('GOT SIZE'):
-      print 'Sending image...'
-      sock.sendall(bytes)
-    
-      # check what server sent
+      
+      # send image ID
+      print 'Sending ID...'
+      sock.sendall('ID %s' % ids[i])
       answer = ""
       try:
-         answer = sock.recv(4096)
-         print 'answer = %s' % answer
+	answer = sock.recv(4096)
+        print 'anser = %s' % answer
       except:
-         sock.close()
+	sock.close()
+
+      # send image
+      if answer.startswith('GOT ID'):
+      	print 'Sending image...'
+      	sock.sendall(bytes)
     
-      if answer.startswith('GOT IMAGE'):
-          sock.sendall("Closing connection\n")
-          sock.close()
-          print 'Image successfully sent to server'
+      	# check server reply
+      	answer = ""
+      	try:
+            answer = sock.recv(4096)
+            print 'answer = %s' % answer
+      	except:
+            sock.close()
+    
+      	if answer.startswith('GOT IMAGE'):
+            sock.sendall("Closing connection\n")
+            sock.close()
+            print 'Image successfully sent to server'
     
     myfile.close()
