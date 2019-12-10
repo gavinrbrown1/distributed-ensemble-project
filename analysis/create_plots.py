1c07e6e3747fdc23901cb9599522ad7a9ec09dad
Gavin Brown <gavinrbrown1@gmail.com>
2019-12-09 19:23:10 -0500

    Started script for analysis

---
+++ analysis/create_plots.py
@@ -0,0 +1,11 @@
+# CS 655 Final Project
+# Iden Kalemaj, Andrea Burns, Gavin Brown
+
+# Script to read in results text files and produce plots
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_in_csv(filename):
+    """read in the results of one experiment, saved in the CSV"""
+    pass
