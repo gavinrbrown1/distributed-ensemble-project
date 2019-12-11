a6d75482c1f3f64daff7a2740bd1b40f2b015f52
Gavin Brown <gavinrbrown1@gmail.com>
2019-12-11 09:43:58 -0500

    Fixed parsing of results CSV

--- analysis/create_plots.py
+++ analysis/create_plots.py
@@ -7,15 +7,19 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('agg')
+import sys
+import re
 
 def read_in_csv(filename):
     """read in the results of one experiment, saved in the CSV"""
+    out = []
     with open(filename, 'r') as f:
         results = [line.split(',') for line in f.read().splitlines()]
-    for i in range(len(results)):
-        results[i][0] = int(results[i][0][13:-4])  # this part depends on how we've saved the image names
-        results[i][1] = int(results[i][1])
-        results[i][2] = float(results[i][2])
+
+    # remove the non-numbers from the file name
+    p = re.compile('\D') # \D matches all non-decimals, equivalent to [^0-9]
+    results = [[int(p.sub('', result[0])), int(result[1]), float(result[2])] for result in results]
+
     return results
 
 def get_true_labels():
@@ -64,19 +68,28 @@ if False:
     plt.legend()
     plt.savefig('comm_time_vs_cache_size.png')
 
+# Plot 1
 # accuracy and error probability
 if True:
     starting_experiment = 0
-    num_exp = 4
+    num_exp = 1
     num_runs = 3 
     total_acc = num_exp*[0]
     probs = [0.0, 0.1, 0.2, 0.3]
     for exp in range(num_exp):
         for run in range(num_runs):
             filename = "../results/results_experiment"+str(starting_experiment+exp)+'_run'+str(run)+'.csv'
+            print(filename)
             results = read_in_csv(filename)
+            print('sample results:')
+            for result in results[:10]:
+                print(result)
+            print()
             acc = accuracy(results, labels)
+            print(acc)
+            print()
             total_acc[exp] += acc
+    sys.exit()
     accs = [acc / num_runs for acc in total_acc]
 
     plt.plot(probs, accs, label='accuracies')
@@ -86,6 +99,67 @@ if True:
     #plt.legend()
     plt.savefig('plot_1.png')
 
+# Plot 2
+# same as above, except random delay
+if False:
+    starting_experiment = 0
+    num_exp = 4
+    num_runs = 3 
+    total_acc = num_exp*[0]
+    delay_mean = [0.1, 0.2, 0.3, 0.4]
+    for exp in range(num_exp):
+        for run in range(num_runs):
+            filename = "../results/results_experiment"+str(starting_experiment+exp)+'_run'+str(run)+'.csv'
+            results = read_in_csv(filename)
+            acc = accuracy(results, labels)
+            total_acc[exp] += acc
+    accs = [acc / num_runs for acc in total_acc]
+
+    plt.plot(delay_mean, accs, label='accuracies')
+    plt.xlabel('Random Delay Mean (s)')
+    plt.ylabel('Average Accuracy')
+    plt.title('Random Delay and Classifier Error')
+    #plt.legend()
+    plt.savefig('plot_2.png')
+
+# Plot 3
+# Communication time, highlighting cache differences
+if False:
+    # uniform 
+    starting_experiment = 0
+    num_exp = 4
+    num_runs = 3 
+    total_comm_time = num_exp*[0]
+    cache_size = [10, 20, 30]
+    for exp in range(num_exp):
+        for run in range(num_runs):
+            filename = "../results/results_experiment"+str(starting_experiment+exp)+'_run'+str(run)+'.csv'
+            results = read_in_csv(filename)
+            time = average_time(results)
+            total_comm_time[exp] += time
+    avg_time = [time / num_runs for time in total_comm_time]
+    plt.plot(cache_size, avg_time, label='Uniform')
+
+    # power
+    starting_experiment = 0
+    num_exp = 4
+    num_runs = 3 
+    total_comm_time = num_exp*[0]
+    cache_size = [10, 20, 30]
+    for exp in range(num_exp):
+        for run in range(num_runs):
+            filename = "../results/results_experiment"+str(starting_experiment+exp)+'_run'+str(run)+'.csv'
+            results = read_in_csv(filename)
+            time = average_time(results)
+            total_comm_time[exp] += time
+    avg_time = [time / num_runs for time in total_comm_time]
+    plt.plot(cache_size, avg_time, label='Power')
+
+    plt.xlabel('Random Delay Mean (s)')
+    plt.ylabel('Average Accuracy')
+    plt.title('Random Delay and Classifier Error')
+    plt.legend()
+    plt.savefig('plot_2.png')
             
 
 
