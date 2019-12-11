efe1db1067d7bddeefdca83ca9f4cc66532f202e
Gavin Brown <gavinrbrown1@gmail.com>
2019-12-11 18:28:52 -0500

    Actually have the scripts download the repo

--- setup_and_instructions/setup_2.sh
+++ setup_and_instructions/setup_2.sh
@@ -2,5 +2,6 @@ echo "Installing pytorch"
 conda install pytorch torchvision cpuonly -c pytorch
 
 echo "Downloading files from Github and unzipping"
+wget distributed-ensemble-project-master.zip
 unzip distributed-ensemble-project-master.zip
 cd distributed-ensemble-project-master
